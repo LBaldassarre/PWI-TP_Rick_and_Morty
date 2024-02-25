889a62e5c89d1a5b9311561e2d906675fae258e0
LBaldassarre <lbaldassarre.dev@gmail.com>
2024-02-25 20:28:55 +0100

    Add style for contact page on big screens + fixed nav position

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -9,20 +9,22 @@ export default function Contact(){
                 <div className="contact-container d-flex flex-column justify-content-center aling-items-center mt-5">
                     <h2 className="text-center text">Contact</h2>
                     <h3 className="text-center mb-2 text">Leave us your information so we can contact you</h3>
-                    <form className="container-form justify-content-md-center p-4">
-                        <div className="d-flex flex-column">
+                    <form className="form-container justify-content-md-center p-4">
+                        <div className="name d-flex flex-column">
                             <label className="mb-2" htmlFor="name">Name</label>
                             <input type="text" className="input rounded-2 p-2 border border-0 mb-2"  aria-label="First name" id="name"/>
                         </div>
-                        <div className="d-flex flex-column">
+                        <div className="email d-flex flex-column">
                             <label className="mb-2" htmlFor="email">Email</label>
                             <input type="email" className="input rounded-2 p-2 border border-0 mb-2"  aria-label="Last name" id="email"/>
                         </div>
-                        <div className="d-flex flex-column">
+                        <div className="message d-flex flex-column">
                             <label htmlFor="message" className="form-label mb-2">Message</label>
                             <textarea className="area rounded-2 p-2 border border-0 mb-2" id="message" cols="30" rows="10"></textarea>
                         </div>
-                        <input className="btn btn-success" type="submit" value="Send"/>
+                        <div className="send">
+                            <input className="btn btn-success" type="submit" value="Send"/>
+                        </div>
                     </form>
                 </div>
             </main>
