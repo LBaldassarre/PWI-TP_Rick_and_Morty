29c5ca372c454df2cdb8cf03f715339f89a5a388
LBaldassarre <lbaldassarre.dev@gmail.com>
2024-02-25 13:01:09 +0100

    Add bigg scren nav style

--- src/components/Navagation/index.js
+++ src/components/Navagation/index.js
@@ -3,19 +3,19 @@ import { Link } from 'react-router-dom';
 export default function Navegation({hoja}){
 
     return(
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
-            <div className="container-fluid">
-                <h1><Link className="navbar-brand" to="/">Rick & Morty</Link></h1>
+        <nav className="nav-container navbar navbar-expand-lg w-100">
+            <div className="container-fluid justify-content-between align-items-center">
+                <Link className="text-decoration-none" to="/"><h1 className="rm-h navbar-brand cursor-p">Rick & Morty</h1></Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
-                    <ul className="navbar-nav">
-                        <li aria-current="page" >
-                            <Link className={hoja === "Characters" ? "nav-link active":"nav-link"} to="/characters">Characters</Link>
+                    <ul className="nav-buttons nav nav-pills">
+                        <li className="nav-item" >
+                            <Link className={hoja === "Characters" ? "nav-link text-decoration-none active":"text-decoration-none nav-link"} to="/characters">Characters</Link>
                         </li>
-                        <li>
-                            <Link className={hoja === "Contact" ? "nav-link active":"nav-link"} to="/contact">Contact</Link>
+                        <li className="nav-item">
+                            <Link className={hoja === "Contact" ? "nav-link text-decoration-none active":"text-decoration-none nav-link"} to="/contact">Contact</Link>
                         </li>
                     
                     </ul>
