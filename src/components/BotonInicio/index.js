8240a06a50470728dac5bb24a9d7159ac316ad5c
LBaldassarre <lbaldassarre.dev@gmail.com>
2024-02-25 11:39:35 +0100

    Add Home Styling

--- src/components/BotonInicio/index.js
+++ src/components/BotonInicio/index.js
@@ -3,10 +3,6 @@ import './BotonInicio.css';
 
 export default function BotonInicio({nombreBoton, pasaje}){
     return(
-        <div className="row navbar align-items-center contenedorboton">
-
-            <Link className="btn-inicio btn " to={pasaje}>{nombreBoton}</Link>
-
-        </div>
+        <Link className="btn-inicio btn " to={pasaje}>{nombreBoton}</Link>
     )
 }
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -4,17 +4,17 @@ import"./Home.css";
 
 export default function Home(){
     return(
-        <div className="principal-search container-fluid d-flex flex-column justify-content-center align-items-center">
+        <div className="container-fluid d-flex flex-column justify-content-center align-items-center vh-100">
         
-            <div className="bg-home container-header d-flex-column justify-content-center">
-                <h1 className="row-justify content-center">Proyect Rick & Morty</h1>
-                <h2 className="text-center p-3">Welcome to Rick & Morty Proyect!</h2>
-                <p className="text-center p-2">This proyect was made for practising React and to made a functional website.</p>
-                <p className="text-center">In this website you can know information of the characters of this animated series.</p>
+            <div className="container-home align-self-center">
+                <h1 className="h-1 text-center">Proyect Rick & Morty</h1>
+                <h2 className="text-center mt-4">Welcome to Rick & Morty Proyect!</h2>
+                <p className="text-center mt-4">This proyect was made for practising React and to made a functional website.</p>
+                <p className="text-center mt-4">In this website you can know information of the characters of this animated series.</p>
                 <p className="text-center">Also you can filter for diferent types of properties to find the character that you are looking for or send us a massage for any concern o sugestion.
                 </p>
-                <p className="text-center p-3">Lets go!</p>
-                <ul className="nav nav-pills justify-content-center">
+                <h2 className="text-center mt-5">Lets go!</h2>
+                <ul className="home-buttons align-self-center justify-self-center d-flex justify-content-center">
                     <BotonInicio nombreBoton='Characters' pasaje="/characters"/>
                     <BotonInicio nombreBoton="Contact" pasaje="/contact"/> 
                 </ul>       
